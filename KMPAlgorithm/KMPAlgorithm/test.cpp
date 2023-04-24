1d838508b6611928becc3dace14a6d309963fa0f
chenlong-xcy <chenlonghyl@163.com>
2023-04-24 20:30:43 +0800

    KMP算法

--- KMPAlgorithm/KMPAlgorithm/test.cpp
+++ KMPAlgorithm/KMPAlgorithm/test.cpp
@@ -27,38 +27,39 @@ vector<int> getNext(const string& needle) {
     //i: 指向后缀末尾位置
     int n = needle.size();
     vector<int> next(n);
-    next[0] = 0; //以第0个字符结尾的字符串的相同前后缀长度为0
-    int j = 0; //前缀末尾位置
-    for (int i = 1; i < n; i++) { //
+    next[0] = 0; //以下标为0的字符结尾的字符串的相同前后缀长度为0
+    int j = 0; //指向前缀末尾位置
+    for (int i = 1; i < n; i++) { //i从1开始,next[0]已被初始化
         while (j > 0 && needle[j] != needle[i]) { //前后缀末尾不相同,前缀位置向前回退
-            j = next[j - 1]; //j回退到之前的子串的相同前后缀的前缀的下一个位置
+            j = next[j - 1]; //j回退到以下标为j-1的字符结尾的字符串的最长相同前后缀的前缀的下一个位置
         }
-        if (needle[j] == needle[i]) { //前后缀末尾相同,前缀位置后移
-            j++; //j后移后的数值也是此时前缀的长度
+        if (needle[j] == needle[i]) { //前后缀末尾相同
+            j++; //j后移后的数值就是此时前缀的长度
         }
-        next[i] = j; //以i为结尾的字符串的相同前后缀的长度（也是前缀的下一个位置的下标）
+        next[i] = j; //以下标为i的字符结尾的字符串的相同前后缀的长度（也是前缀的下一个位置的下标）
     }
-    return next;
+    return next; //返回前缀表
 }
+
 int searchNeedle(string haystack, string needle) {
-    //j: 遍历模式串
-    //i: 遍历文本串
+    //j: 指向模式串中下一个待比较的字符
+    //i: 指向文本串中下一个待比较的字符
     int n = needle.size();
-    if (n == 0)
+    if (n == 0) //模式串为空串
         return 0;
-    vector<int> next = getNext(needle);
-    int j = 0;
-    for (int i = 0; i < haystack.size(); i++) {
-        while (j > 0 && needle[j] != haystack[i]) { //当前字符不匹配,j向前回退（重新寻找匹配位置）
-            j = next[j - 1];
+    vector<int> next = getNext(needle); //根据模式串生成前缀表
+    int j = 0; //指向模式串中下一个待比较的字符
+    for (int i = 0; i < haystack.size(); i++) { //指向文本串中下一个待比较的字符
+        while (j > 0 && needle[j] != haystack[i]) { //当前字符不匹配,指向模式串的指针向前回退（重新寻找匹配位置）
+            j = next[j - 1]; //回退到以下标为j-1的字符结尾的字符串的最长相同前后缀的前缀的下一个位置
         }
-        if (needle[j] == haystack[i]) { //当前字符匹配,j后移
-            j++;
+        if (needle[j] == haystack[i]) { //当前字符匹配
+            j++; //指向模式串的指针后移
             if (j == n) //j已经到达模式串末尾,匹配完毕
-                return i - n + 1;
+                return i - n + 1; //返回文本串中的起始匹配下标
         }
     }
-    return -1;
+    return -1; //匹配失败,文本串中没有出现过模式串
 }
 
 //时间复杂度: O(n+m)
@@ -69,3 +70,4 @@ int main() {
     cout << searchNeedle("aabaabaafa", "aabaaf") << endl;
 	return 0;
 }
+
